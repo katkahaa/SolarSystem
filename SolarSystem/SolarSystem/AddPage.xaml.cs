f9a006bba2c26cce9e035162819a1a2e41ff5fbc
Katerina Haasova <haasova.katerina@email.cz>
2022-05-09 06:06:03 +0200

    addpage + solarsystempage

---
+++ SolarSystem/SolarSystem/AddPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SolarSystem
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddPage : ContentPage
+    {
+        public AddPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSend_Clicked(object sender, EventArgs e)
+        {
+            SolarSystemPage.PlanetsList.Add(new Planet { Category = EntryCategory.Text, Name = EntryName.Text, AboutPlanet = EntryAboutPlanet.Text });
+        }
+    }
+}
---
+++ SolarSystem/SolarSystem/SolarSystemPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SolarSystem
+{
+    public class Planet
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string AboutPlanet { get; set; }
+    }
+    public partial class SolarSystemPage : ContentPage
+    {
+        public static ObservableCollection<Planet> PlanetsList { get; set; }
+        public SolarSystemPage()
+        {
+            InitializeComponent();
+            PlanetsList = new ObservableCollection<Planet>(
+                new Planet[]
+                {
+                    new Planet {Category="Planet", Name="Earth", AboutPlanet="Blue planet"},
+                    new Planet {Category="Dwarf planet", Name="Pluto", AboutPlanet="Pluto (minor-planet designation: 134340 Pluto) is a dwarf planet in the Kuiper belt, a ring of bodies beyond the orbit of Neptune. It was the first object discovered in the Kuiper belt and remains the largest known body in that area. After Pluto was discovered in 1930, it was declared the ninth planet from the Sun."},
+                });
+            PlanetsListView.ItemsSource = PlanetsList;
+        }
+        private void PlanetsListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            DisplayAlert((PlanetsListView.SelectedItem as Planet).Category + (PlanetsListView.SelectedItem as Planet).Name, (PlanetsListView.SelectedItem as Planet).AboutPlanet, "OK");
+        }
+    }
+}
